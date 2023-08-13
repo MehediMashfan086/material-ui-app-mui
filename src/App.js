5a21efbb495dd1da303b8b7c74e1d05a283ae517
Mehedi Hasan <mehedihasan62086@gmail.com>
2023-08-13 14:29:18 +0600

    move the formpage in the component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
-import { TextField, Stack, Box, Button } from '@mui/material';
 import './App.css';
+import FormPage from './components/FormPage';
 
 function App() {
   return (
     <div className="App">
-      <br />
-      <Box component="form" sx={{
-        "margin": "1%",
-        "border": "2px solid #ccc",
-        // ".MuiInputBase-root": {"width": "130ch"},
-        // ".MuiButtonBase-root": {"width": "130ch"}
-      }}>
-        <Stack direction={'column'} spacing={3} margin={3}>
-          <TextField label="User Name" />
-          <TextField label="Password" type='password' />
-          <TextField label="Mobile" type='tel' />
-          <TextField label="Email" type='email' />
-          <TextField label="Address" multiline maxRows={3} minRows={2} />
-          <TextField label="Date Of Birth" type='date' 
-            InputLabelProps={{
-            shrink: true,
-            }}
-          />
-          <Button variant="contained">Submit</Button>
-        </Stack>
-      </Box>
+      <FormPage />
     </div>
   );
 }
---
+++ src/components/FormPage.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { TextField, Stack, Box, Button } from '@mui/material';
+
+const FormPage = () => {
+  return (
+    <div>
+        <br />
+        <Box component="form" sx={{
+            "margin": "1%",
+            "border": "2px solid #ccc",
+            // ".MuiInputBase-root": {"width": "130ch"},
+            // ".MuiButtonBase-root": {"width": "130ch"}
+        }}>
+            <Stack direction={'column'} spacing={3} margin={3}>
+            <TextField label="User Name" />
+            <TextField label="Password" type='password' />
+            <TextField label="Mobile" type='tel' />
+            <TextField label="Email" type='email' />
+            <TextField label="Address" multiline maxRows={3} minRows={2} />
+            <TextField label="Date Of Birth" type='date' 
+                InputLabelProps={{
+                shrink: true,
+                }}
+            />
+            <Button variant="contained">Submit</Button>
+            </Stack>
+      </Box>
+    </div>
+  )
+}
+
+export default FormPage
