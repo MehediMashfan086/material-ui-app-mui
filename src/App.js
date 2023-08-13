462a1501c730e1c7ebbb986e415b17839d4c512d
Mehedi Hasan <mehedihasan62086@gmail.com>
2023-08-13 14:14:50 +0600

    cerate(submission from): with TextField Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { TextField, Stack, Box, Button } from '@mui/material';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br />
+      <Box component="form" sx={{
+        "margin": "1%",
+        "border": "2px solid #ccc",
+        // ".MuiInputBase-root": {"width": "130ch"},
+        // ".MuiButtonBase-root": {"width": "130ch"}
+      }}>
+        <Stack direction={'column'} spacing={3} margin={3}>
+          <TextField label="User Name" />
+          <TextField label="Password" type='password' />
+          <TextField label="Mobile" type='tel' />
+          <TextField label="Email" type='email' />
+          <TextField label="Address" multiline maxRows={3} minRows={2} />
+          <TextField label="Date Of Birth" type='date' 
+            InputLabelProps={{
+            shrink: true,
+            }}
+          />
+          <Button variant="contained">Submit</Button>
+        </Stack>
+      </Box>
     </div>
   );
 }
